773fe0b540db7c1826dc89575a017d5c0eae038a
Everest K.C <babueverest@gmail.com>
2014-10-02 18:40:16 +0545

    swingx

--- himss/src/main/java/com/whiteclover/himss/Database.java
+++ himss/src/main/java/com/whiteclover/himss/Database.java
@@ -3,7 +3,6 @@ package com.whiteclover.himss;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
