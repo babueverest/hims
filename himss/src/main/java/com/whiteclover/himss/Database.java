992a6ab834dc86afc50aa1f014ee98039f57ca30
Everest K.C <babueverest@gmail.com>
2014-10-02 16:35:24 +0545

    dbase1

--- himss/src/main/java/com/whiteclover/himss/Database.java
+++ himss/src/main/java/com/whiteclover/himss/Database.java
@@ -6,6 +6,7 @@ package com.whiteclover.himss;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -20,13 +21,14 @@ public class Database {
 	Class.forName(driver);
         Connection c;
         try{
-            String url = "jdbc:derby:himsb";
+            String url = "jdbc:derby:himsa";
             c = DriverManager.getConnection(url);
+            System.out.println(c);
             return c;
         }catch(SQLException ex){
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Creating database");
-            String url = "jdbc:derby:himsb;create=true";
+            String url = "jdbc:derby:himsa;create=true";
             c = DriverManager.getConnection(url);
             create_table(c);
             return c;
@@ -37,21 +39,39 @@ public class Database {
       public static void create_table(Connection cn) throws SQLException {
           Statement st=cn.createStatement();
           st.executeUpdate("create table buy_details (date_of_purchase date, product_name varchar(100),quantity int,rate int,supplier_name varchar(150))");
-          st.executeUpdate("cteate table product_details (product_id int, product_name varchar(100), quantity int)");
-          st.executeUpdate("create table sales_details (date_of_sales date, customer_name varchar(100), productID int ,quantity int,rate int");
+          st.executeUpdate("create table product_details (product_id integer not null generated always as identity (start with 1, increment by 1), product_name varchar(100), quantity int,CONSTRAINT primary_key PRIMARY KEY (product_id))");
+          st.executeUpdate("create table sales_details (date_of_sales date, customer_name varchar(100), productID int ,quantity int,rate int)");
           st.close();
       }
 
-      public static void insert_buy_details(Connection cn,Buy_Details buy_details) throws SQLException{
+      public static int insert_buy_details(Connection cn,Buy_Details[] buy_details) throws SQLException{
           Statement st=cn.createStatement();
-          st.executeUpdate("insert into buy_details (product_id,..) values ()");
-
+          int auto_num=0;
+          for(int i=0;i<=buy_details.length;i++){
+          st.executeUpdate("insert into buy_details (date_of_purchase,product_name,quantity,rate,supplier_name) values ("+buy_details[i].getDate_of_Purchase()+","+buy_details[i].getProduct_Name()+","+buy_details[i].getQuantity()+","+buy_details[i].getRate()+","+buy_details[i].getSupplier_Name()+")",Statement.RETURN_GENERATED_KEYS);
+          }
+          return auto_num;
+      }
+      
+      public static void add_product_details(Connection cn,Product_Details product_details) throws SQLException{
+          Statement st=cn.createStatement();
+          ResultSet result_set=st.executeQuery("select * from product_details where name="+product_details.getProduct_Name());
+          if(result_set.next()){ //exists update
+              st.executeUpdate("update product_details set quantity=(quantity+"+product_details.getQuantity()+") where name="+"'"+product_details.getProduct_Name()+"'");
+          }
+          else{
+          st.executeUpdate("insert into product_details (product_name, quantity) values()");
+          }
+      }
+      
+      public static void deduct_product_quantity(Connection cn, Product_Details product_details) throws SQLException{
+          
       }
 
       public static void insert_sales_details(Connection cn,SalesDetail[] salesdetail) throws SQLException{
           Statement st=cn.createStatement();
           for(int i=0;i<=salesdetail.length;i++){
-          st.executeUpdate("insert into sales_details (date_of_sales date, customer_name, productID, quantity, rate) values ("+
+          st.executeUpdate("insert into sales_details (date_of_sales, customer_name, productID, quantity, rate) values ("+
                            salesdetail[i].getDate()+","+salesdetail[i].getCustomer_name()+","+salesdetail[i].getProductID()+","
                            +salesdetail[i].getQuantity()+","+salesdetail[i].getRate()+")");
           st.close();
@@ -75,6 +95,7 @@ public class Database {
         try {
             // TODO code application logic here
             Connection cn = createDatabaseConnection();
+            cn.close();
 
         } catch (    SQLException | ClassNotFoundException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
